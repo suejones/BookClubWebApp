ae92f5545895362deebea217f61cb0814f8798b1
suejones <suemileyjones@gmail.com>
2018-10-25 17:56:43 +0100

    Create roles in Startup

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using WebApplication2.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication2.Startup))]
 namespace WebApplication2
@@ -10,7 +13,70 @@ namespace WebApplication2
         {
             ConfigureAuth(app);
         }
+        // In this method we will create default User roles and Admin user for login   
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new Models.ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
 
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("BCService"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "BCService";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "BCApp";
+                user.Email = "BCApp@gmail.com";
+
+                string userPWD = "BC@711711";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "BCService");
+
+                }
+            }
+
+            // Creating BookClubAdmin role    
+            if (!roleManager.RoleExists("BCAdmin"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "BCAdmin";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating BookClubMember role    
+            if (!roleManager.RoleExists("BCMember"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "BCMember";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Library role    
+            if (!roleManager.RoleExists("BCMember"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "BCMember";
+                roleManager.Create(role);
+
+            }
+
+        }
     }
 }
+
